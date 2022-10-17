4c31f1daf9c241f7766ebc5566341ddfe472adb2
NguyenDuyHieu987 <vaicut6941@gmail.com>
2022-10-17 09:34:14 +0700

    add

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -6,21 +6,24 @@ const db = require('../db');
 
 module.exports = {
   get: (req, res) => {
-    let sql = 'SELECT * FROM products';
+    let sql = `SELECT * FROM products LIMIT 6 OFFSET ${req.query.offset}`;
     db.query(sql, (err, response) => {
       if (err) throw err;
       res.json(response);
     });
   },
   getType: (req, res) => {
-    let sql = `SELECT danh_muc FROM products`;
+    let sql = `SELECT danh_muc FROM products GROUP BY danh_muc`;
     db.query(sql, (err, response) => {
       if (err) throw err;
       res.json(response);
     });
   },
   getByType: (req, res) => {
-    let sql = `SELECT * FROM products WHERE danh_muc = '${req.query.type}' `;
+    console.log(req.query.offset);
+    console.log(req.query.type);
+
+    let sql = `SELECT * FROM products WHERE danh_muc = '${req.query.type}' LIMIT 6 OFFSET ${req.query.offset}`;
     db.query(sql, (err, response) => {
       if (err) throw err;
       res.json(response);
